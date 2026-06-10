60fae74a79c18478176ae60d44d32206831daf17
toeypnptp <tonpum_p@silpakorn.edu>
2026-06-10 16:02:04 +0700

    toey commit

--- tests/Runbike Registration.spec.ts
+++ tests/Runbike Registration.spec.ts
@@ -6,7 +6,7 @@ const USERNAME = '0888614919';
 const PASSWORD = 'P@ssw0rd';
 
 //test james
-
+//test toey
 test('1. Register runbike.', async ({ page }) => {
   await login(page, USERNAME, PASSWORD);
   await expect(page.getByRole('heading',{name: 'ประวัติการลงทะเบียน'})).toBeVisible();
